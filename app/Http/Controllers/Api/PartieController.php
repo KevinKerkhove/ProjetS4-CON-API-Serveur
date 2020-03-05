d2b2222bed0ec1fffcace300990de6b86ad629b0
Kévin Avot <kevin_avot@ens.univ-artois.fr>
2020-03-05 15:58:54 +0100

    Controller 3

--- app/Http/Controllers/Api/PartieController.php
+++ app/Http/Controllers/Api/PartieController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Modeles\Partie;
 use Illuminate\Http\Request;
 
 class PartieController extends Controller
 {
-    //
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->only(['score','time','ennemiesKilled','id']);
+
+        $partie = new Partie();
+
+        $partie -> score = $input['score'];
+        $partie -> time = $input['time'];
+        $partie -> ennemiesKilled = $input['ennemiesKilled'];
+        $partie-> player_id = $input['id'];
+
+
+        $partie->save();
+    }
 }
