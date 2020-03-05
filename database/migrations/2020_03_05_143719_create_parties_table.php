f76cab367d98003b703e84c580b3f410169c3b76
[maxence.boisedu] <maxence_boisedu@ens.univ-artois.fr>
2020-03-05 15:43:11 +0100

    pirehgoerjo

--- database/migrations/2020_03_05_143719_create_parties_table.php
+++ database/migrations/2020_03_05_143719_create_parties_table.php
@@ -17,6 +17,7 @@ class CreatePartiesTable extends Migration
             $table->bigIncrements('id');
             $table->integer('score');
             $table->integer('time');
+            $table->integer('ennemiesKilled');
             $table->timestamps();
         });
     }
