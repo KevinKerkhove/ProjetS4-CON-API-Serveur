7a645d931e68a1275190cb45bf698cc2ea59228e
[maxence.boisedu] <maxence_boisedu@ens.univ-artois.fr>
2020-03-05 15:39:24 +0100

    2 migrations

---
+++ database/migrations/2020_03_05_143116_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('playerName');
+            $table->integer('playTime')->nullable();
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
