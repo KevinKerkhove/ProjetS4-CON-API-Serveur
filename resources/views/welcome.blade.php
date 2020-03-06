b866424e613a1d32891695be9af34aecaf466490
Kévin Avot <kevin_avot@ens.univ-artois.fr>
2020-03-06 11:04:38 +0100

    accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
         </style>
     </head>
     <body>
-    <h1>Bienvenue sur la MOULAGANG</h1>
+    <h1>Chronicles of Naruberaria</h1>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
