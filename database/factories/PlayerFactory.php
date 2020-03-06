723b4650b2c0c3a60eeeaa717a3dfd1bd6e5c0e9
Kévin Avot <kevin_avot@ens.univ-artois.fr>
2020-03-06 14:08:28 +0100

    factory player

---
+++ database/factories/PlayerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Modeles\Personne;
+use Faker\Generator as Faker;
+
+$factory->define(Player::class, function (Faker $faker) {
+    $createAt = $faker->dateTimeInInterval(
+        $startDate = '-6 months',
+        $interval = '+ 180 days',
+        $timezone = date_default_timezone_get()
+    );
+    return [
+        'playerName' => $faker->name(),
+        'bestScore' => $faker->numberBetween(0,5000),
+        'playTime' => $faker->numberBetween(0,360000),
+        'created_at' => $createAt,
+        'updated_at' => $faker->dateTimeInInterval(
+            $startDate = $createAt,
+            $interval = $createAt->diff(new DateTime('now'))->format("%R%a days"),
+            $timezone = date_default_timezone_get()
+        ),
+    ];
+});
