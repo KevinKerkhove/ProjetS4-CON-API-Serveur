31d3bbaf11455edac6dafde3a0b02d2aa291ac3d
Kévin Avot <kevin_avot@ens.univ-artois.fr>
2020-03-06 10:50:12 +0100

    routes api

--- routes/api.php
+++ routes/api.php
@@ -17,16 +17,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('login', 'Api\AuthController@login');
-Route::post('register', 'Api\PersonneController@store');
+Route::post('register', 'Api\PlayerController@store');
 
-Route::post('personnes', 'Api\PersonneController@store');
+Route::post('players', 'Api\PlayerController@store');
 
 Route::group(['middleware' => 'auth.jwt'], function () {
     Route::post('logout', 'Api\AuthController@logout');
     Route::get('me', 'Api\AuthController@me');
     Route::post('refresh', 'Api\AuthController@refresh');
-    Route::get('personnes', 'Api\PersonneController@index');
-    Route::get('personnes/{id}', 'Api\PersonneController@show');
-    Route::put('personnes/{id}', 'Api\PersonneController@update');
-    Route::delete('personnes/{id}', 'Api\PersonneController@destroy');
+    Route::get('players', 'Api\PlayerController@index');
+    Route::get('players/{id}', 'Api\PlayerController@show');
+    Route::put('players/{id}', 'Api\PlayerController@update');
+    Route::delete('players/{id}', 'Api\PlayerController@destroy');
 });
--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-Route::resource('games', 'GameController');
-
-Route::resource('tags', 'TagController');
-
-
-Route::post('/comment', 'CommentaireController@store')->name('comment.store');
-
-Route::get('/comment/edit/{id}', 'CommentaireController@edit')->name('comment.edit');
-
-Route::get('/comment/update/{id}', 'CommentaireController@update')->name('comment.update');
-
-Route::get('/comment/destroy/{id}', 'CommentaireController@destroy')->name('comment.destroy');
-
-Route::get('/comment/create/{id}', 'CommentaireController@create')->name('comment.create');
-
-Route::prefix('accueil')->group(function(){
-    Route::get('/','AccueilController@index')->name('accueil.index');
-    Route::get('apropos','AccueilController@apropos')->name('accueil.apropos');
-    Route::get('contacts','AccueilController@contacts')->name('accueil.contact');
-});
-
 Auth::routes();
 
 Route::get('/logout','UserController@logout')->name('logout');
